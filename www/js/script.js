ee5fe7df21c525903b887ae0e65d1afcefceb09e
Glomberg <Bazz@bk.ru>
2016-10-18 12:08:59 +0300

    starting

---
+++ www/js/script.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function(){
+	$("a.scroll").on("click", function(e){
+		e.preventDefault();
+		href = $(this).attr("href");
+		if($(href).length != 0) {
+			$("body,html").stop().animate({ scrollTop: $(href).offset().top - 110}, 1000);
+		}
+	});
+});
